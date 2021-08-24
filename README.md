d392b6dc0ccb46ec82e34bd9fbffef8dcd04e173
hyouinKyouma <adarsh.adarshsaxena@gmail.com>
2021-08-24 20:23:41 +0530

    added the business logic and tested

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 ## json file transaltor
 - Using the node script a json file can be converted to any other language supported by LibreTranslate.
+
+- Install libretranslate link: https://github.com/LibreTranslate/LibreTranslate#mirrors
+
+- run it on the local server.
+
+- modify the variables at the top of translator.js
+
+- run the file using node.
+
--- translator.js
+++ translator.js
@@ -1,106 +1,76 @@
-// const axios = require('axios');
-function main(){
-    const fs = require('fs');
-const efile = {  "Change": "Change",
-"Remove": "Remove",
-"Year": "Year",
-"Add Holiday": "Add Holiday",
-"Remove Holiday": "Remove Holiday",
-"Description": "Description",
-"Included holiday": "Included holiday",
-"With checking the box the holiday will be considered in the facility schedule.": "With checking the box the holiday will be considered in the facility schedule.",
-"Holiday": "Holiday",
-"Cancel": "Cancel",
-"Yes": "Yes",
-"No": "No",
-"Insurance List": "Insurance List",
-"Create Insurance": "Create Insurance",
-"Remove Insurances": "Remove Insurances",
-"Show": "Show",
-"Insurance": "Insurance"}
+const axios = require('axios');
+const fs = require('fs');
+const fsPromises = require('fs').promises;
+  
+const pathInputjsonfile = './h0.json'
+const pathoutputjsonfile = './h2.json'
+let inputdata = {}
+let outputobj = {}
+const sourceLanguage =  "en"
+const targetLanguage = "hi"
+const url = 'http://localhost:5000/translate'
 
-
-const hfile = { "बदलें": "बदलें",
-"निकालें": "निकालें",
-"वर्ष": "वर्ष",
-"छुट्टी जोड़ें": "छुट्टी जोड़ें",
-"छुट्टी निकालें": "छुट्टी निकालें",
-"विवरण": "विवरण",
-"शामिल छुट्टी": "शामिल छुट्टी",
-"बॉक्स को चेक करने से अवकाश को सुविधा अनुसूची में माना जाएगा।": "बॉक्स को चेक करने से अवकाश को सुविधा अनुसूची में माना जाएगा।",
-"हॉलिडे": "हॉलिडे",
-"रद्द करें": "रद्द करें",
-"हाँ":" हाँ",
-"नहीं":" नहीं",
-"बीमा सूची": "बीमा सूची",
-"बीमा बनाएं": "बीमा बनाएं",
-"बीमा निकालें": "बीमा निकालें",
-"दिखाओ":" दिखाओ",
-"बीमा": "बीमा" 
-}
-// var a = 0
-
-// axios.post(url, {
-//     firstName: 'Fred',
-//     lastName: 'Flintstone'
-//   })
-//   .then(function (response) {
-//     console.log(response);
-//   })
-//   .catch(function (error) {
-//     console.log(error);
-//   });
-
-function replaceValue(o1) {
-    var a = 0
-    for (let key in o1) {
-        if (o1.hasOwnProperty(key)) {
-            a += 1
-            o1[key] = String(a)
+async function inputdataectiterator(){
+    for(let key in inputdata){
+        if(inputdata.hasOwnProperty(key)){
+            let convertedResponse = await callTranslator(inputdata[key])
+            outputobj[key] = convertedResponse["data"]["translatedText"]
+            // console.log("================>",outputobj)
+        
         }
     }
-    return o1
-}
+    console.log("================>",outputobj)
+}     
 
-function replaceKeys(o2) {
-    let a = 0
-    let newObj = {}
-    for (let key in o2) {
-        if (o2.hasOwnProperty(key)){
-            a += 1
-            newObj[String(a)] = o2[key]
-        }
+async function callTranslator(value){
+    let body = {
+
+        "q":value,
+        "source": sourceLanguage,
+        "target": targetLanguage
     }
-    return newObj
-}
-
-function createFinalObject(eng,hin){
-    for( let keyi in eng){
-        for ( let keyj in hin){
-            if( eng[keyi] == keyj){
-                eng[keyi] = hin[keyj]
-            }
-        }
+    try{
+        let response = await  axios.post(url, body)
+        return response
+    }catch(error){
+        console.error("ERROR----->"+ error)
     }
-    return eng
+    
 }
 
-    let newObje = replaceValue(efile)
-    let newHindiObj = replaceKeys(hfile)
-    let newHinglishObj = createFinalObject(newObje,newHindiObj)
-    // let efile2 = JSON.stringify(newHinglishObj)
-    // console.log(efile2)
-    fs.readFile('./h1.json', 'utf8', (err,data) =>{
+function toreadfile(){
+
+    return new Promise(function (resolve, reject) {
+        fs.readFile(pathInputjsonfile, 'utf8', function(error, contents) {
+          if (error) reject(error);
+          else resolve(contents);
+        });
+      });
+
+    
+}
+
+async function main(){
+
+    inputdata = await toreadfile()
+    inputdata = JSON.parse(inputdata)
+    // inputdata = {
+    //     "Included holiday": "Included holiday",
+    //     "With checking the box the holiday will be considered in the facility schedule.": "With checking the box the holiday will be considered in the facility schedule."
+    // }
+    // console.log(fileobj)
+    await inputdataectiterator()
+    fs.readFile(pathoutputjsonfile, 'utf8', (err,data) =>{
         if(err){
             console.log(err)
         }else{
             let readObj = JSON.parse(data)
             totalObj = { 
                 ...readObj, 
-                ...newHinglishObj
+                ...outputobj
                 }
             newjson = JSON.stringify(totalObj)
-            fs.writeFile('./h1.json', newjson, 'utf8', (err) => {
+            fs.writeFile(pathoutputjsonfile, newjson, 'utf8', (err) => {
 
                 if (err) {
                     console.log(`Error writing file: ${err}`);
@@ -111,6 +81,8 @@ function createFinalObject(eng,hin){
             });
         }
     })
+    
+    
    
 
 }
